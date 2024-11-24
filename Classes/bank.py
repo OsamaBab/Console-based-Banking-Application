c4324573694cf9534fd042729ba7d73178656222
OsamaBab <gorglak8@gmail.com>
2024-11-24 08:03:38 +0000

    Modified bank class

--- Classes/bank.py
+++ Classes/bank.py
@@ -3,16 +3,15 @@ class Bank:
     Discription:
     Represents the overall banking system, managing customers, accounts, 
     and the persistence of data.
+        Attributes:
+            bank_name (str): Name of the bank.
+            phone (int): Bank phone number.
+            address (str): the address of the bank
     """
     def __init__(myself, bank_name, phone, address):
 
         """
-        Initialise a Bank instance.
-
-        Args:
-            bank_name (str): Name of the bank.
-            phone (int): Bank phone number.
-            address (str): the address of the bank
+        Initialise a Bank instance.  
         """
         myself.bank_name = bank_name
         myself.phone = phone
