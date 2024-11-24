f376bcbd1aca22305764dd9f0003e04a642e207b
OsamaBab <gorglak8@gmail.com>
2024-11-24 08:04:36 +0000

    Modified customer class notation

--- Classes/customers.py
+++ Classes/customers.py
@@ -5,17 +5,17 @@ class Customer:
     Discription:
     A class represents a bank customer details, associated accounts, 
     and login credentials.
-    """
-    def __init__(myself, cust_id, name, password=None, email=None, cust_accounts=None):
-        """
-        Discription:
-        Initialize a new Customer instance.
         Args:
             cust_id (str): Unique customer identifier for each customer.
             name (str): Full name of the customer.
             password (str): Encrypted password for customer login.
             email (str): Email address of the customer for login.
             cust_accounts ([]): List of associated Account instances for each customer. Default is None or an empty list.
+    """
+    def __init__(myself, cust_id, name, password=None, email=None, cust_accounts=None):
+        """
+        Discription:
+        Initialise the Customer instance.
         """
         myself.cust_id = cust_id
         myself.name = name
