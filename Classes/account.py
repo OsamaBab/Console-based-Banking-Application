7f3145cafa94bcf1a056834ab93702f7eec69df6
OsamaBab <gorglak8@gmail.com>
2024-11-24 08:03:13 +0000

    Modified account class

--- Classes/account.py
+++ Classes/account.py
@@ -8,20 +8,19 @@ class Account:
     Discription:
     Represents a bank account with attributes like balance, account type, customer ID, 
     and associated transaction history.
+        Attributes:
+            account_number (str): Account number. Generated if not provided.
+            balance (float): Initial balance. Default is 0.
+            account_type (str): Type of account (e.g., "savings", "current"). Default is "savings".
+            cust_id (str): ID of the customer associated with the account.
+            transactions (list): List of transaction. dafult is none value
     """
 
     def __init__(myself, acc_number=None, balance=0, account_type="savings", cust_id=None, transactions=None):
         
         """
         Discription:
-        Initialise a new Account instance from account class.
-
-        Args:
-            account_number (str): Account number. Generated if not provided.
-            balance (float): Initial balance. Default is 0.
-            account_type (str): Type of account (e.g., "savings", "current"). Default is "savings".
-            cust_id (str): ID of the customer associated with the account.
-            transactions (list): List of transaction. dafult is none value
+        Initialise a new Account instance for account class.
         """
         myself.acc_number = acc_number or myself.generate_account_number()
         myself.balance = balance
@@ -31,11 +30,9 @@ class Account:
 
         def generate_account_number(myself):
             """
-            Generate a random 6-digit account number."""
-            """
-            Generate a unique account number.
+            Generate a random and unique account number
 
-            Returns:
-                str: A randomly generated 6-digit account number.
+                Returns:
+                str: A randomly generated 7-digit account number.
             """
             return ''.join(random.choices(string.digits, k=7))
